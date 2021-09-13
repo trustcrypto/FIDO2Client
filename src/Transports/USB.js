d170c5e93dc4fdf77b65521d8b819e53d58c49d3
Bradley Matusiak <bmatusiak@gmail.com>
2021-09-13 17:08:16 -0400

    Update USB.js

--- src/Transports/USB.js
+++ src/Transports/USB.js
@@ -167,8 +167,8 @@ class Device {
         let firstFrame = payload.slice(0, this.maxPacketSize - 7);
         let currentPayloadOffset = this.maxPacketSize - 7;
         let firstPacket = new InitPacket(this.maxPacketSize, this.cid, cmd, payload.length, firstFrame);
-        let b;
-        if(process.env.OS.toLowerCase().indexOf("window") >= -1)
+        var b;
+        if(process.env.OS && process.env.OS.toLowerCase().indexOf("window") >= -1)
             b = [0].concat(Array.from(firstPacket.toBytes()))
         else b = firstPacket.toBytes();
         this.deviceHandle.write(b);
@@ -178,9 +178,10 @@ class Device {
             let nextFrame = payload.slice(currentPayloadOffset, currentPayloadOffset + packetSize);
             currentPayloadOffset += packetSize;
             let nextPacket = new ContPacket(this.cid, this.maxPacketSize, seq, nextFrame);
-            if(process.env.OS.toLowerCase().indexOf("window") >= -1)
+            var b;
+            if(process.env.OS && process.env.OS.toLowerCase().indexOf("window") >= -1)
                 b = [0].concat(Array.from(nextPacket.toBytes()))
-            else b = firstPacket.toBytes();
+            else b = nextPacket.toBytes();
             this.deviceHandle.write(b);
             seq += 1;
         }
